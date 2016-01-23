f492c03e29c6c796a0e3211b27027ee51f790a40
Vlado <vladimir.pechev@abv.bg>
2016-01-23 10:08:21 +0200

    adding tests directory

---
+++ test/utilities/TestUtility.java
@@ -0,0 +1,15 @@
+package utilities;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestUtility {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		assertEquals(1, 1);
+	}
+
+}
